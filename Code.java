82de36b7980456c0a40c81b8a539b9d9634feaaf
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2023-07-23 22:04:13 +0300

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,21 +1,52 @@
-public class Rectangle {
-    double width;
-    double height;
+abstract class Shape {
+    public abstract double area();
 }
-...
-public class Geometry {
-    double area(Object shape) {
-        if (shape instanceof Circle) {
-            Circle circle = (Circle) shape;
-            return Math.PI * circle.radius * circle.radius
-        } else if (shape instanceof Rectangle) {
-            Rectangle rectangle = (Rectangle) shape;
-            return rectangle.width * rectangle.height;
-        } else if (shape instanceof Square) {
-            Square square = (Square) shape;
-            return square.size * square.size;
-        }
- 
-        throw new IllegalArgumentException("Unknown shape");
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
+
+class Square extends Shape {
+
+    private double length;
+
+    public Square(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double area() {
+        return length * length;
+    }
+}
+
+class Geometry{
+    public double get_area(Shape shape){
+        return shape.area();
     }
 }
